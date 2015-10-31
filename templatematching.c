67f1ca07d393e8c79ea7a4bf4fd2b2ae1d3b3248
induarun9086 <indu9086@gmail.com>
2015-10-31 18:38:12 +0100

    mode added for filtering

--- templatematching.c
+++ templatematching.c
@@ -43,9 +43,9 @@ int main(int argc, char* argv[])
 	struct matrix filter;
 	struct matrix filteredImage;
 
-	initMatrix(&image,5,5);
-	initMatrix(&filter,3,3);
-	initMatrix(&filteredImage,5,5);
+	initMatrix(&image,7,7);
+	initMatrix(&filter,5,5);
+	initMatrix(&filteredImage,7,7);
 
 	float value = 1.0;
 
@@ -53,47 +53,76 @@ int main(int argc, char* argv[])
 		for (uint8 j = 0; j < image.numberOfColumns; ++j) {
 			MAT(&image, i, j) = value;
 			value++;
-
 		}
-
 	}
+
+#ifdef DEBUG
+	printMatrix(&image);
+#endif
 
 	MAT(&filter,0,0) = 1.0;
 	MAT(&filter,0,1) = -1.0;
 	MAT(&filter,0,2) = 3.0;
+	MAT(&filter,0,3) = 3.0;
+	MAT(&filter,0,4) = 3.0;
 	MAT(&filter,1,0) = -1.0;
 	MAT(&filter,1,1) = 5.0;
 	MAT(&filter,1,2) = -1.0;
+	MAT(&filter,1,3) = -1.0;
+	MAT(&filter,1,4) = -1.0;
 	MAT(&filter,2,0) = 0.0;
 	MAT(&filter,2,1) = -1.0;
 	MAT(&filter,2,2) = 0.0;
+	MAT(&filter,2,3) = 0.0;
+	MAT(&filter,2,4) = 0.0;
+	MAT(&filter,3,0) = 0.0;
+	MAT(&filter,3,1) = -1.0;
+	MAT(&filter,3,2) = 0.0;
+	MAT(&filter,3,3) = 0.0;
+	MAT(&filter,3,4) = 0.0;
+	MAT(&filter,4,0) = 0.0;
+	MAT(&filter,4,1) = -1.0;
+	MAT(&filter,4,2) = 0.0;
+	MAT(&filter,4,3) = 0.0;
+	MAT(&filter,4,4) = 0.0;
 
-	applyFilter(&image,&filter,&filteredImage,1);
+	applyFilter(&image,&filter,&filteredImage,CORRELATION_OPERATION,MODE_SYMMETRIC);
 
 	return 0;
 }
 
-void applyFilter(struct matrix* image,struct matrix* filter,struct matrix* filteredImage,uint8 operation){
+void applyFilter(struct matrix* image,struct matrix* filter,struct matrix* filteredImage,uint8 operation,uint8 mode){
 
 	float tempResult ;
 	float tempPixelValue = 0.0;
+#ifdef DEBUG
+	struct matrix tempMatrix;
+	initMatrix(&tempMatrix,filter->numberOfRows,filter->numberOfColumns);
+#endif
 	for (uint16 i = 0; i < image->numberOfRows; ++i) {
 		for (uint16 j = 0; j < image->numberOfColumns; ++j) {
 				tempResult = 0.0;
 				for (int l = 0; l < filter->numberOfRows; ++l) {
 					for (int m = 0; m < filter->numberOfColumns; ++m) {
-						tempPixelValue = getPixelValue(image,i,j,l-(filter->numberOfRows/2),m-(filter->numberOfColumns/2));
+						tempPixelValue = getPixelValue(image,i,j,l-(filter->numberOfRows/2),m-(filter->numberOfColumns/2),mode);
+#ifdef DEBUG
+						MAT(&tempMatrix,l,m) = tempPixelValue;
+#endif
 						if(operation == CONVOLUTION_OPERATION)
 						{
-							tempResult = tempResult + (tempPixelValue*MAT(filter,(filter->numberOfRows-1-l),(filter->numberOfColumns-1-m)));
+							tempResult = tempResult + (tempPixelValue * MAT(filter,(filter->numberOfRows-1-l),(filter->numberOfColumns-1-m)));
 						}
 						else
 						{
-							tempResult = tempResult + (tempPixelValue*MAT(filter,l,m));
+							tempResult = tempResult + (tempPixelValue * MAT(filter,l,m));
 						}
 					}
 				}
+#ifdef DEBUG
+				printMatrix(&tempMatrix);
+#endif
 				MAT(filteredImage,i,j) = tempResult;
+
 		}
 	}
 
@@ -101,8 +130,10 @@ void applyFilter(struct matrix* image,struct matrix* filter,struct matrix* filte
 }
 
 float getPixelValue(struct matrix* image,uint32 imgPosX,uint32 imgPosY,
-		uint32 filterPosX,uint32 filterPosY)
+		uint32 filterPosX,uint32 filterPosY,uint8 mode)
 {
+	float pixelValue = 0.0;
+
 	sint32 imgCurrentPosX = imgPosX+filterPosX;
 	sint32 imgCurrentPosY = imgPosY+filterPosY;
 	if((imgCurrentPosX < 0) ||
@@ -110,12 +141,33 @@ float getPixelValue(struct matrix* image,uint32 imgPosX,uint32 imgPosY,
 		(imgCurrentPosY < 0) ||
 		(imgCurrentPosY > (image->numberOfColumns-1)))
 	{
-			return 0.0;
+			if(mode == MODE_REPLICATE)
+			{
+				imgCurrentPosX = ((imgCurrentPosX < 0) ? 0 : (imgCurrentPosX > image-> numberOfRows-1) ? (image-> numberOfRows-1) : imgCurrentPosX);
+				imgCurrentPosY = ((imgCurrentPosY < 0) ? 0 : (imgCurrentPosY > image-> numberOfColumns-1) ? (image-> numberOfColumns-1) : imgCurrentPosY);
+
+				pixelValue = MAT(image,imgCurrentPosX,imgCurrentPosY);
+			}
+			else if(mode == MODE_SYMMETRIC)
+			{
+				if(imgCurrentPosX < 0 || imgCurrentPosX > image->numberOfRows-1)
+				{
+					imgCurrentPosX = (image->numberOfRows -1) - (mod(imgCurrentPosX,image->numberOfRows));
+				}
+				if(imgCurrentPosY < 0 || imgCurrentPosY > image->numberOfColumns-1)
+				{
+					imgCurrentPosY = (image->numberOfColumns -1) - (mod(imgCurrentPosY,image->numberOfColumns));
+				}
+
+				pixelValue = MAT(image,imgCurrentPosX,imgCurrentPosY);
+			}
 	}
 	else
 	{
-		return MAT(image,imgCurrentPosX,imgCurrentPosY);
+		pixelValue = MAT(image,imgCurrentPosX,imgCurrentPosY);
 	}
+
+	return pixelValue;
 
 }
 
@@ -142,11 +194,28 @@ void printMatrix(struct matrix* mat)
 	printf("\n --------------------------------------- \n");
 	for (i = 0; i < mat->numberOfRows; ++i) {
 			for (j = 0; j < mat->numberOfColumns; ++j) {
-				printf("%f " ,MAT(mat,i,j));
+				printf("%10.3f " ,MAT(mat,i,j));
 			}
 			printf("\n");
 	}
 
+}
+
+uint32 mod (sint32 a, sint32 b)
+{
+	sint32 ret = 0;
+   if(a >= 0)
+   {
+	   ret =  a % b;
+   }
+   else
+   {
+	   ret = a;
+	   while(ret < 0)
+	        ret+=b;
+   }
+
+   return (uint32)ret;
 }
 
 
--- templatematching.h
+++ templatematching.h
@@ -20,6 +20,11 @@ typedef signed int sint16;
 #define CONVOLUTION_OPERATION (0)
 #define CORRELATION_OPERATION (1)
 
+#define MODE_PAD_ZERO (0)
+#define MODE_SYMMETRIC (1)
+#define MODE_REPLICATE (2)
+#define MODE_CIRCULAR (3)
+
 uint8 imageData[TEMPLATEMATCHING_IMAGE_LENGTH];
 
 struct matrix{
@@ -28,14 +33,17 @@ struct matrix{
 	float* values;
 };
 
-void applyFilter(struct matrix* image,struct matrix* filter,struct matrix* filteredImage,uint8 operation);
+void applyFilter(struct matrix* image,struct matrix* filter,struct matrix* filteredImage,uint8 operation,uint8 mode);
 
-float getPixelValue(struct matrix* image,uint32 imgPosX,uint32 imgPosY,uint32 filterPosX,uint32 filterPosY);
+float getPixelValue(struct matrix* image,uint32 imgPosX,uint32 imgPosY,uint32 filterPosX,uint32 filterPosY,uint8 mode);
 
 void initMatrix(struct matrix* mat,uint32 numRows,uint32 numColumns);
 
 void destroyMatrix(struct matrix* mat);
 
 void printMatrix(struct matrix* mat);
+
+uint32 mod (sint32 a, sint32 b);
+
 
 #endif /* TEMPLATEMATCHING_H_ */
